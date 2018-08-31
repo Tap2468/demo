77d3ced98ac50acc469e6fd9a7addd3f35a81182
Tap2468 <42852983+Tap2468@users.noreply.github.com>
2018-08-31 12:17:44 -0400

    Updated from Brython Server: 8/31/2018 12:17:43 PM

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print(this is my first program")
+"""
+helloworld.py
+Author: Patrick Daley
+Credit: I didn't use any other sources
+Assignment:
+Write and submit a Python program that prints the following:
+Hello, world!
+"""
+print("Hello World")
